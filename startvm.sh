7e198fb4771be228a27d1a747efb52bb6fe4d6cd
Paul Michali <pcm@cisco.com>
2013-10-07 17:25:17 +0000

    Added third I/F so that management is not used for private net

--- startvm.sh
+++ startvm.sh
@@ -7,8 +7,9 @@ if [ -f .instance_info ]; then
    rm .instance_info
 fi
 
-IFNAME_ETH0=$NAME"__private"
+IFNAME_ETH0=$NAME"__mgmt"
 IFNAME_ETH1=$NAME"__public"
+IFNAME_ETH2=$NAME"__private"
 # You add interfaces on aditional networks
 # by specifying an IFNAME_ETHx line. The
 # word following the '__' is the name of the
@@ -29,6 +30,8 @@ kvm -m 8192 -name $NAME \
 -net tap,ifname=$IFNAME_ETH0,vlan=0,script=osn-ifup-br-int,downscript=osn-ifdown-br-int \
 -net nic,macaddr=$MACADDR_ETH1,model=e1000,vlan=1 \
 -net tap,ifname=$IFNAME_ETH1,vlan=1,script=osn-ifup-br-ex,downscript=osn-ifdown-br-ex \
+-net nic,macaddr=$MACADDR_ETH2,model=e1000,vlan=2 \
+-net tap,ifname=$IFNAME_ETH2,vlan=2,script=osn-ifup-br-int,downscript=osn-ifdown-br-int \
 -drive file=$IMAGE \
 -boot c \
 -vga cirrus \
